bb7a9a4130c406698359c1b2d7460a67387b745f
cagrikilic <cakilic@mix.wvu.edu>
2020-08-25 02:27:31 -0400

    change subscriber name from driving to manipulation

--- motion_control/src/cmd_joints.cpp
+++ motion_control/src/cmd_joints.cpp
@@ -7,11 +7,11 @@
 
 JointsCommander::JointsCommander(ros::NodeHandle & nh)
 : nh_(nh)
-{    
-  
+{
+
   // Create a subscriber.
   // Name the topic, message queue, callback function with class name, and object containing callback function.
-  sub_ = nh_.subscribe("driving/arm_joint_angles", 1000, &JointsCommander::messageCallback, this);
+  sub_ = nh_.subscribe("manipulation/arm_joint_angles", 1000, &JointsCommander::messageCallback, this);
 
   // Node publishes individual joint positions
   pub1_ = nh_.advertise<std_msgs::Float64>("mount_joint_controller/command", 1000);
@@ -68,9 +68,9 @@ int main(int argc, char **argv)
 
   // Create a new JointsCommander object.
   JointsCommander joints_commander(nh);
-  
+
   ros::spin();
-  
+
   return 0;
 
 } // end main()
